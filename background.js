87a373522a13fb9febedcd012340fe5507867826
Alex <wandong_yan@brown.edu>
2023-08-10 18:38:03 -0400

    Building popup, implementing Bookmark functions

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// A listener for when the tab is updated, and if the url contains "youtube.com/watch", it will send a message to the content script with the videoId.
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes("youtube.com/watch")) {
       const queryParameters = tab.url.split("?")[1];
@@ -5,8 +6,7 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
   
       chrome.tabs.sendMessage(tabId, {
         type: "NEW",
-        videoId: urlParameters.get("v"),
+        videoId: urlParameters.get("v"), // Get the videoId from the url ("v=videoId")
       });
     }
-  });
-  
+});
